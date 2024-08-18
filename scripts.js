53de239dff7fd6861e8af4495830079ee853d192
erdqc-droid <erdem098baran@gmail.com>
2024-08-18 19:12:20 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-// Example JavaScript to handle smooth scrolling for navigation links
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
